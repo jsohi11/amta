14b6e15795e210f43c2aa818dafc3cfb0a09a6e5
Jacinth Sohi <jacinthsohi@gmail.com>
2016-01-10 22:41:50 -0800

    Update db to postgres from sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160111031203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "subscribers", force: :cascade do |t|
     t.string   "phone_number"
     t.boolean  "subscribed"
