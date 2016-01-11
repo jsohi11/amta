526aeac3862c13407af154cf0547ade94a747b58
Jacinth Sohi <jacinthsohi@gmail.com>
2016-01-10 23:14:18 -0800

    Changed SID

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,7 +3,7 @@ class Subscriber < ActiveRecord::Base
   # Notify our appointment attendee X minutes before the appointment time
   def send_message(msg, image_url)
     @twilio_number = ENV['+19093452682']
-    @client = Twilio::REST::Client.new ENV['AC7de7ef54a0668e0d73f626dcd25c784c'], ENV['e008c7d0d652fd4a662a4dc0f40bb1c8']
+    @client = Twilio::REST::Client.new ENV['PN7b4b300d2872f8200908f9983d06f0b9'], ENV['e008c7d0d652fd4a662a4dc0f40bb1c8']
     message = @client.account.messages.create(
       :from => @twilio_number,
       :to => self.phone_number,
